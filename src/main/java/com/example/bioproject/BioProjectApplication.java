791298203b6303f519edac2609d877a065a779ce
OskarThormar <osth0002@stud.kea.dk>
2024-10-03 11:35:48 +0200

    imports i main

--- src/main/java/com/example/bioproject/BioProjectApplication.java
+++ src/main/java/com/example/bioproject/BioProjectApplication.java
@@ -3,13 +3,11 @@ package com.example.bioproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.bioproject.model.Movie;
-import com.example.bioproject.repository.BioRepository;
-
 @SpringBootApplication
 public class BioProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BioProjectApplication.class, args);
     }
+
 }
