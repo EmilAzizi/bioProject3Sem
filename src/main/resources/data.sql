5183f0021a570a02deb5688b93d66aa054e71bd3
OskarThormar <osth0002@stud.kea.dk>
2024-10-04 11:15:49 +0200

    compose.yaml done

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,5 +19,3 @@ CREATE TABLE IF NOT EXISTS movie (
 ('Interstellar', 'Matthew McConaughey, Anne Hathaway, Jessica Chastain', 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity''s survival.', true, '2014-11-07', '2024-11-07', 169),
 ('The Matrix', 'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss', 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.', true, '1999-03-31', '2009-03-31', 136),
 ('The Shawshank Redemption', 'Tim Robbins, Morgan Freeman, Bob Gunton', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', true, '1994-09-23', '2004-09-23', 142);
-    
-    
